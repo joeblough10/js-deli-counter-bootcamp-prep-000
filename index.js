f3f9c92d8ad0ac0d13c0334077460d36c2e6c05a
alfie8000@gmail.com <alfie8000@gmail.com>
2018-06-26 18:12:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 var katzDeli = [];
 
+
+
+function takeANumber(katzDeliLine){
+  
+}
+
+// takeANumber(katzDeliLine)
+
+
+
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
